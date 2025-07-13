7c5187d02beba57fc95009058564338be749edc1
obinna <michaelobinna35@gmail.com>
2025-07-13 12:33:32 +0100

    fix: align dropdown menu to right on mobile view

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Certificate = () => {
   return (
     <div>
-      <div style={{marginBottom:'600px'}} >
+      <div style={{marginBottom:'300px'}} >
               <p style={{color:'#bdbdbd', fontSize:'40px', lineHeight:'1px'}} >Company</p>
               <h1 style={{color:'#333333', fontSize:'40px',lineHeight:'1px'}}>Certifications</h1>
         </div>
